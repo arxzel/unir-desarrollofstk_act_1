b8b10b9b83c20b660a5b739be9422aca3b4af5c1
Arxzel <arxzel@gmail.com>
2023-05-14 19:26:37 -0500

    Cambio del index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,11 +6,13 @@ function Index() {
     <h1>Actividad 1</h1>
     <p>Actividad para la materia de desarrollo full stack,</p>
     <p>author: Fabián Andrés Milláj Jaimes</p>
-    <p>Ejercicio propuesto: Una plataforma para gestionar un listado de estudiantes, cursos y docentes
-        La intenión es realizar un crud para los estudiantes, cursos y docentes, así mismo relacionar entre sí.
-        Ejemplo: El curso de desarrollo full stack, tiene un docente, y muchos estudiantes.
+    <p>Ejercicio propuesto: Una plataforma para gestionar un listado de cursos, estudiantes y personas
+        La intenión es realizar un crud para los las personas y cursos, posteriormente relacionarlos entre sí.
+        Ejemplo: El curso de desarrollo full stack, tiene un docente, y muchos estudiantes (Funcionalidad futura).
         Así mismo, el docente puede ser maestro de otros cursos.
         Los estudiantes pueden estar listados en varios cursos.
+        El alcance de este proyecto, tiene como ejercicio desarrollar los conocimientos adquiridos en la clase
+        de desarrollo fullstack, implementando componentes, hooks y props en angular
     </p>
   </div>;
 }
